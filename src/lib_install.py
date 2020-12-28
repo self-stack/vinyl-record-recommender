a374d62726cb6611b8d7683754571241f02b52c0
self-stack <53274146+zacharyksb@users.noreply.github.com>
2020-12-28 10:12:40 -0600

    testing on ubuntu

--- src/lib_install.py
+++ src/lib_install.py
@@ -1,8 +1,12 @@
 import sys
 import subprocess
 
+#TODO: add pip3 install
+# add pandas
+# add pymongo
+# add tqdm
 subprocess.check_call([sys.executable, '-m', 'pip3', 'spotipy'])
 subprocess.check_call([sys.executable, '-m', 'pip3', 'lyricsgenius'])
 #subprocess.check_call([sys.executable, '-m', 'pip3', 'textblod
-subprocess.check_call([sys.executable, '-m', 'pip3', 'essential_generators'])
+subprocess.check_call([sys.executable, '-m', 'pip3', 'essential-generators'])
 #pip3 install pyLDAvis
